7256b8daae987efd768fd86930e7cb75219aa959
Maha <mahaalqarni.offficial@gmail.com>
2024-03-27 00:53:26 +0300

    done

--- Planteer/main/urls.py
+++ Planteer/main/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path("plants/update/<plant_id>/", views.update_plant, name="update_plant"),
     path("plants/delete/<plant_id>/", views.delete_plant, name="delete_plant"),
     path("plants/search/", views.plant_search, name="plant_search"),
-    path("contact/", views.contact_us, name="contact_us"),
+    # path("contact/", views.contact_us, name="contact_us"),
     path("comments/add/<plant_id>/", views.add_comment, name="add_comment"),
 ]
